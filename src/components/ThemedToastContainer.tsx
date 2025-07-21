25aadada535aa79e292289ee718e5df356cb8f0f
ChanceBarimbao <chancevillasbarimbao@gmail.com>
2025-07-21 16:08:28 -0700

    Updated toast notifs

--- src/components/ThemedToastContainer.tsx
+++ src/components/ThemedToastContainer.tsx
@@ -11,11 +11,11 @@ const ThemedToastContainer = () => {
       position="top-right"
       autoClose={3000}
       hideProgressBar={false}
-      newestOnTop
-      closeOnClick
+      newestOnTop={true}
+      closeOnClick={true}
       rtl={false}
       pauseOnFocusLoss
-      draggable
+      draggable={true}
       pauseOnHover
       theme={theme.palette.mode === 'dark' ? 'dark' : 'light'}
     />

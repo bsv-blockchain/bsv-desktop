231ddff66ea4a6475943366c6ca6517563cbfbbb
ChanceBarimbao <chance.barimbao@oit.edu>
2025-09-03 12:40:19 -0700

    added a get balance helper function

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,45 +2,15 @@ import { useState, useEffect, useContext, useCallback } from 'react'
 import AmountDisplay from './AmountDisplay'
 import { Skeleton, Stack, Typography } from '@mui/material'
 import { WalletContext } from '../WalletContext'
+import { getAccountBalance } from "../utils/getAccountBalance"
 
 const Profile = () => {
-  const { managers, adminOriginator } = useContext(WalletContext)
-  const [accountBalance, setAccountBalance] = useState<number | null>(null)
-  const [balanceLoading, setBalanceLoading] = useState(true)
-
-  const refreshBalance = useCallback(async () => {
-    try {
-      if (!managers?.permissionsManager) {
-        return
-      }
-      setBalanceLoading(true)
-      const limit = 10000
-      let offset = 0
-      let allOutputs = []
-
-      // Fetch the first page
-      const firstPage = await managers.permissionsManager.listOutputs({ basket: 'default', limit, offset }, adminOriginator)
-      allOutputs = firstPage.outputs;
-      const totalOutputs = firstPage.totalOutputs;
-
-      // Fetch subsequent pages until we've retrieved all outputs
-      while (allOutputs.length < totalOutputs) {
-        offset += limit;
-        const { outputs } = await managers.permissionsManager.listOutputs({ basket: 'default', limit, offset }, adminOriginator);
-        allOutputs = allOutputs.concat(outputs);
-      }
-
-      const total = allOutputs.reduce((acc, output) => acc + output.satoshis, 0)
-      setAccountBalance(total)
-      setBalanceLoading(false)
-    } catch (e) {
-      setBalanceLoading(false)
-    }
-  }, [managers, adminOriginator])
-
-  useEffect(() => {
-    refreshBalance()
-  }, [refreshBalance])
+  const { managers} = useContext(WalletContext)
+  const {
+    balance: accountBalance,
+    loading: balanceLoading,
+    refresh: refreshBalance,
+  } = getAccountBalance("default");
 
   return (<Stack alignItems="center">
     <Typography variant='h5' color='textSecondary' align='center'>
@@ -60,4 +30,4 @@ const Profile = () => {
   </Stack>)
 }
 
-export default Profile
+export default Profile;
